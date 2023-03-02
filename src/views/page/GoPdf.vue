55778e2c2e7fa27b5ea8bfcc246a2ea58c716f35
Thilina <devt@outlook.com>
2023-03-02 16:20:38 +0530

    b

--- src/views/page/GoPdf.vue
+++ src/views/page/GoPdf.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="content g2-content">
-    <object :data="url" type="application/pdf" name="file.pdf">
+    <!-- <object :data="url" type="application/pdf" name="file.pdf">
       <embed :src="url" type="application/pdf" />
-    </object>
+    </object> -->
+    <pdf src="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"></pdf>
+
+
   </div>
 </template>
 
 <script>
 import { decode64 } from "@utils/AcrouUtil";
+import pdf from 'vue-pdf';
+
 export default {
+  components: {
+    pdf
+  },
   data: function() {
     return {};
   },
   computed: {
     url() {
       if (this.$route.params.path) {
+        console.log(this.$route.params.path);
+        console.log(decode64(this.$route.params.path));
         return decode64(this.$route.params.path);
       }
       return ''
