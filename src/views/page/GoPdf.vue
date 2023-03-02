ca6206c01d266ef10df9941a4918ab7e10580e03
Thilina <devt@outlook.com>
2023-03-02 16:48:58 +0530

    f

--- src/views/page/GoPdf.vue
+++ src/views/page/GoPdf.vue
@@ -3,28 +3,18 @@
     <object :data="url" type="application/pdf" name="file.pdf">
       <embed :src="url" type="application/pdf" />
     </object>
-    <!-- <pdf src="./path/to/static/relativity.pdf"></pdf> -->
-<h1>{{url}}</h1>
-
   </div>
 </template>
 
 <script>
 import { decode64 } from "@utils/AcrouUtil";
-// import pdf from 'vue-pdf'
-
 export default {
-  // components: {
-  //   pdf
-  // },
   data: function() {
     return {};
   },
   computed: {
     url() {
       if (this.$route.params.path) {
-        // console.log(this.$route.params.path);
-      
         return decode64(this.$route.params.path);
       }
       return ''
